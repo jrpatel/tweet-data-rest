864695b6682c8a439fb43ceff8328f20112ad784
Jinal Patel <jinalpatel1212@gmail.com>
2018-05-23 12:52:00 -0400

    with timing measurement

--- src/main/java/com/mit/tweets/mit_fluetweets_service/TweetController.java
+++ src/main/java/com/mit/tweets/mit_fluetweets_service/TweetController.java
@@ -16,20 +16,23 @@ import org.springframework.web.client.RestTemplate;
 import com.sun.javafx.collections.MappingChange.Map;
 @RestController
 public class TweetController {
-	
+	RestTemplate restTemplate = new RestTemplate();
 	@RequestMapping("/tweets")
 	public Response[] getTweets() {
 		
-		RestTemplate restTemplate = new RestTemplate();
+		System.out.println("execution started!!!!!");
+		long starttime = System.currentTimeMillis();
 		Tweet[] tweets = restTemplate.getForObject("http://api.flutrack.org/?limit=10", Tweet[].class);
-		WeatherResponse weather =getLocation(tweets[0].getLongitude(), tweets[0].getLongitude());
-		System.out.println(weather.toString().getClass());
-		System.out.println(weather.getClass());
+		System.out.println("-----> time took to get tweets "+(System.currentTimeMillis() - starttime));
 		Response[] response = new Response[tweets.length];  
+		System.out.println("starting to prepare the response");
+		long starttime2 = System.currentTimeMillis();
         for(int i=0;i<response.length;i++) {
 			
 			response[i] = getResponse(tweets[i]);
 		}
+        System.out.println("-----> processing response took time"+(System.currentTimeMillis()-starttime2));
+        System.out.println("----> total time "+(System.currentTimeMillis()-starttime));
 		return response;
 		/////Threading ///////
 		/*ExecutorService executor = Executors.newFixedThreadPool(2);
@@ -103,35 +106,46 @@ public class TweetController {
 	
 	
 	private WeatherResponse  getLocation(String lonn, String latt) {
-		RestTemplate restTemplate = new RestTemplate();
+	//RestTemplate restTemplate = new RestTemplate();
+		
+		long starttime = System.currentTimeMillis();
 		Woeid[] woeid = restTemplate.getForObject("https://www.metaweather.com/api/location/search/?lattlong="+latt+","+lonn, Woeid[].class);
+		System.out.println("----> took to find location"+(System.currentTimeMillis()-starttime));
 		System.out.println("may be retrived is"+woeid[0]);
 		try {
+		long s2 = System.currentTimeMillis();
 		WeatherResponse weather = restTemplate.getForObject("https://www.metaweather.com/api/location/"+woeid[0], WeatherResponse.class);
+		System.out.println("----> took time to get the weather details"+(System.currentTimeMillis()-s2));
 		System.out.println(weather);
+		System.out.println("---> total time took "+(System.currentTimeMillis()-starttime));
 		return weather;
 		}
 		catch(Exception e) {
 			System.out.println(e.getMessage());
+			return null;
 		}
-		return null;
+		//return null;
 	}
 	
 	private Response getResponse(Tweet tweet) {
+		long stime = System.currentTimeMillis();
 		Response res = new Response();
 		WeatherResponse weather =getLocation(tweet.getLongitude(), tweet.getLongitude());
-		System.out.println(weather.toString().getClass());
-		System.out.println(weather.getClass());
+		System.out.println("--> time took for 1 response"+(System.currentTimeMillis()-stime));
+		//System.out.println(weather.toString().getClass());
+		//System.out.println(weather.getClass());
+		long btime = System.currentTimeMillis();
 		List<Consolitaded_Weather>  temp = weather.getConsolidated_weather();
 		res.setAvgMax( temp.stream().mapToDouble(Consolitaded_Weather::getMax_temp).average());
-		res.setAvgMin(temp.stream().mapToDouble(Consolitaded_Weather::getMax_temp).average());
+		res.setAvgMin(temp.stream().mapToDouble(Consolitaded_Weather::getMin_temp).average());
 		res.setUser_name(tweet.getUser_name());
 		res.setTweet_text(tweet.getTweet_text());
 		res.setLatitude(tweet.getLatitude());
 		res.setLongitude(tweet.getLongitude());
 		res.setTweet_date(tweet.getTweet_date());
 		res.setAggravation(tweet.getAggravation());
-		System.out.println("here coming"+res.getAvgMax());
+		System.out.println("----> time took post processing response"+(System.currentTimeMillis()-btime));
+		//System.out.println("here coming"+res.getAvgMax());
 		return res;
 		
 	}
